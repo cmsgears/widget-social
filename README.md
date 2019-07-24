a7bc976cf70866a8984d2e09164fdad7698ddfa6
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-07-24 15:48:25 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+Project 	- CMSGears (https://www.cmsgears.org)
+
+Widget  	- Social
+
+License 	- GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+
+Description - The Social widget for CMSGears provides widgets to show social feeds, counts and to share the page on social networks.
+
+Highlighted Features
+-------------------
+1. Instagram Widget can be used to show most recent Instagram Posts of an Account. It needs a valid Instagram Access Token.
+2. Tweets Widget can be used to show most recent Tweets of an Account. It also need SNS Connect Plugin of CMSGears to access Twitter configuration.
+3. Facebook Widget can be used to show most recent Posts. It also need SNS Connect Plugin of CMSGears to access Facebook configuration.
+4. The Counts Widget can be used to show social counts for specified social networks and supported by the widget.
+5. The Social Share Widget allows site users to share on social networks.
---
+++ share/views/default.php
@@ -0,0 +1,39 @@
+<ul class="nav">
+    <?php if( array_key_exists( 'reddit', $widget->links ) ) { ?>
+       <li>
+		   <a title="Share on Reddit" class="share-facebook <?= $widget->iconSet ?>reddit color-facebook" href="https://reddit.com/submit?url=<?= $widget->url ?>" target="_blank"></a>
+	   </li>
+    <?php } ?>
+	<?php if( array_key_exists( 'facebook', $widget->links ) ) { ?>
+       <li>
+		   <a title="Share on Facebook" class="share-facebook <?= $widget->iconSet ?>facebook color-facebook" href="http://www.facebook.com/sharer.php?u=<?= $widget->url ?>" target="_blank"></a>
+	   </li>
+    <?php } ?>
+    <?php if( array_key_exists( 'pinterest', $widget->links ) ) { ?>
+       <li>
+		   <a title="Share on Pinterest" class="share-pinterest <?= $widget->iconSet ?>pinterest color-facebook" href="http://pinterest.com/pin/create/link/?url=<?= $widget->url ?>" target="_blank"></a>
+	   </li>
+    <?php } ?>
+
+    <?php if( array_key_exists( 'twitter', $widget->links ) ) { ?>
+       <li>
+		<a title="Share on Twitter" class="<?= $widget->iconSet ?>twitter color-twitter" href="https://twitter.com/share?url=<?= $widget->url ?>" target="_blank"></a>
+	   </li>
+    <?php } ?>
+
+    <?php if( array_key_exists( 'linkedin', $widget->links ) ) { ?>
+       <li>
+		   <a title="Share on LinkedIn" class="<?= $widget->iconSet ?>linkedin color-linkedin" href="http://www.linkedin.com/shareArticle?url=<?= $widget->url ?>" target="_blank"></a>
+	   </li>
+    <?php } ?>
+    <?php if( array_key_exists( 'tumblr', $widget->links ) ) { ?>
+       <li>
+		   <a title="Share on Tumblr" class="share-tumblr <?= $widget->iconSet ?>tumblr color-facebook" href="https://www.tumblr.com/widgets/share/tool?canonicalUrl=<?= $widget->url ?>" target="_blank"></a>
+	   </li>
+    <?php } ?>
+    <?php if( array_key_exists( 'mix', $widget->links ) ) { ?>
+       <li>
+		   <a title="Share on Mix" class="share-mix <?= $widget->iconSet ?>mix color-facebook" href="http://mix.com/submit?url=<?= $widget->url ?>" target="_blank"></a>
+	   </li>
+    <?php } ?>
+</ul>
